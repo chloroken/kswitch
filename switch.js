b1984193e3a2bb4c0b718728f7d42388e39c5072
chloroken <72576960+chloroken@users.noreply.github.com>
2025-10-08 16:52:03 +0000

    Update switch.js

--- switch.js
+++ switch.js
@@ -2,7 +2,7 @@ var windows = workspace.windowList();
 
 for (var i = 0; i < windows.length; ++i) {
   var w = windows[i];
-  if (w.resourceClass == "steam_app_8500") {
+  if (w.resourceClass == "steam_app_8500") { //"steam_default_app" for lutris
     if (w.captionNormal != "$WINDOWNAME") {
       w.minimized = true;
       w.fullScreen = false;
